dfd960410a4ea81d8df8403de87c7f493b0c0684
OdisseasKorovesis <odkorovesis@gmail.com>
2021-06-11 18:39:20 +0300

    completed jwt authentication process

--- src/main/java/com/odkor/projectmanagementtool/security/JwtTokenProvider.java
+++ src/main/java/com/odkor/projectmanagementtool/security/JwtTokenProvider.java
@@ -1,8 +1,7 @@
 package com.odkor.projectmanagementtool.security;
 
 import com.odkor.projectmanagementtool.domain.User;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -34,5 +33,31 @@ public class JwtTokenProvider {
                 .signWith(SignatureAlgorithm.HS512, SecurityConstants.SECRET)
                 .compact();
 
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SecurityConstants.SECRET).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            System.out.println("Invalid JWT signature.");
+        } catch (MalformedJwtException ex) {
+            System.out.println("Invalid JWT token.");
+        } catch (ExpiredJwtException ex) {
+            System.out.println("Expired JWT token.");
+        } catch (UnsupportedJwtException ex) {
+            System.out.println("Unsupported JWT token.");
+        } catch (IllegalArgumentException ex) {
+            System.out.println("JWT claims string is empty.");
+        }
+
+        return false;
+
+    }
+
+    public Long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser().setSigningKey(SecurityConstants.SECRET).parseClaimsJws(token).getBody();
+        String id = (String) claims.get("id");
+        return Long.parseLong(id);
     }
 }
